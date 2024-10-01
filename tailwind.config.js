04cafcdc92763a3e7458e2b0b8f254c6de671da9
yeon <yeonnnnn.k@gmail.com>
2024-10-01 15:55:16 +0900

    🎉프로젝트 생성 및 초기 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/**/*.{js,jsx,ts,tsx}', // TailwindCSS가 적용될 파일 경로 설정
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
